8505888ec0d30f99261410076e23235fa67d84ab
leel-swivel <leel@swivelgroup.com.au>
2023-04-12 11:34:38 +0530

    Add Tests

---
+++ src/test/java/com/hilltop/authservice/config/ResourceBundleMessageSourceBeanTest.java
@@ -0,0 +1,37 @@
+package com.hilltop.authservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceBundleMessageSourceBeanTest {
+
+
+    @Test
+     void messageSourceShouldHaveConfiguredBasenames() {
+        ResourceBundleMessageSourceBean bean = new ResourceBundleMessageSourceBean();
+        MessageSource messageSource = bean.messageSource();
+        assertNotNull(messageSource);
+        assertTrue(true);
+        ResourceBundleMessageSource rbms = (ResourceBundleMessageSource) messageSource;
+        String[] basenames = rbms.getBasenameSet().toArray(new String[0]);
+        assertArrayEquals(new String[]{"success", "error"}, basenames);
+    }
+
+    @Test
+     void messageSourceShouldHaveConfiguredEncoding() {
+        ResourceBundleMessageSourceBean bean = new ResourceBundleMessageSourceBean();
+        MessageSource messageSource = bean.messageSource();
+        assertNotNull(messageSource);
+    }
+
+    @Test
+     void messageSourceShouldUseCodeAsDefaultMessage() {
+        ResourceBundleMessageSourceBean bean = new ResourceBundleMessageSourceBean();
+        MessageSource messageSource = bean.messageSource();
+        assertNotNull(messageSource);
+    }
+
+}
